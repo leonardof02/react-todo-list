1cd5d2cd4f58abbd25d088fd392f8b4fb89ae13f
leonardof02 <leonardoarencibia2002@gmail.com>
2023-07-15 18:42:49 -0400

    Styles of main app implemented (dark-theme)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-function App() {
+import desktopBackgroundImage from "./assets/images/bg-desktop-dark.jpg";
+import mobileBackgroundImage from "./assets/images/bg-mobile-dark.jpg";
 
-  return (
-    <>
-        <div>
-            Hello World!
-        </div>
-    </>
-  )
+import moonIcon from "./assets/images/icon-moon.svg";
+import sunIcon from "./assets/images/icon-sun.svg";
+
+import "./index.css";
+import "./styles/dark-theme.css";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <main>
+                <img
+                    className="background-image"
+                    src={mobileBackgroundImage}
+                    alt="Background Picture"
+                />
+                <div className="todo-list-container">
+                    <div className="separated-flex">
+                        <h1>TODO</h1>
+                        <button>
+                            <img src={sunIcon} alt="Sun icon" />
+                        </button>
+                    </div>
+                    < TodoList />
+                </div>
+            </main>
+        </>
+    );
 }
 
-export default App
+export default App;
