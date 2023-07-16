c4647f6acae095b863fa57895bd035626e9d4358
leonardof02 <leonardoarencibia2002@gmail.com>
2023-07-15 20:20:19 -0400

    Finish the style of entire app for mobile

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,6 +11,14 @@ export default function TodoList() {
                 <Todo title="Casa de mima" />
                 <Todo title="Casa de mima" />
             </div>
+            <div className="box flex-center options">
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+            <div className="flex-center" style={{margin: "2rem"}}>
+                <p>Drag and drop to reorder list</p>
+            </div>
         </div>
     );
 }
